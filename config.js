cb881e3c3abf28c33371d0d8fc93b25f96d991b1
KermRayan <kermd237@gmail.com>
2024-12-13 12:00:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ global.SESSION_ID =
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || ".",
-  AUTO_LIKE_STATUS: LIKE_EMOJI || "😻",
+  AUTO_LIKE_STATUS: LIKE_EMOJI || "😻",// put random emoji
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`BRAND PRODUCT OF ☞⌜ KG TECH⌝☜`",
